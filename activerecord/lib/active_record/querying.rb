fe61e32da29aebfcbcc46b33503e9692612b96ab
raghu nandan <raghu.nandan@freshworks.com>
2024-12-10 14:46:57 +0530

    test

--- activerecord/lib/active_record/querying.rb
+++ activerecord/lib/active_record/querying.rb
@@ -44,7 +44,8 @@ module ActiveRecord
     #   Post.find_by_sql ["SELECT title FROM posts WHERE author = ? AND created > ?", author_id, start_date]
     #   Post.find_by_sql ["SELECT body FROM comments WHERE author = :user_id OR approved_by = :user_id", { :user_id => user_id }]
     def find_by_sql(sql, binds = [], preparable: nil, &block)
-      puts "|||||| sql: #{sql}"
+      puts "|||||| sql: #{sql.inspect}"
+      puts "|||||| name: #{name}"
       puts "|||||| binds: #{binds.inspect}"
       puts "|||||| preparable: #{preparable.inspect}"
       result_set = connection.select_all(sanitize_sql(sql), "#{name} Load", binds, preparable: preparable)
